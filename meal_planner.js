3968ec28e9038c0f51181010ba8b57c471f96224
raizelfs <83401657+raizelfs@users.noreply.github.com>
2021-05-12 22:21:47 +0100

    second commit

--- meal_planner.js
+++ meal_planner.js
@@ -1,39 +1,20 @@
-const meals = {
-    breakfast: ["porrigde", "muesli", "pancakes", "eggs on toast", "fruit and yoghurt"],
-    lunch: ["vegtable soup", "a mixed veg and hummous sandwich", "a tuna salad", "a smoked salmon bagel", "sushi"],
-    dinner: ["pasta and pesto", "roasted beans and veg", "sweet potatoe chips", "salmon and broccoli", "rissoto"]
-};
+ let breakfast= ["porrigde", "muesli", "pancakes", "eggs on toast", "fruit and yoghurt"]
+    let lunch =["vegtable soup", "a mixed veg and hummous sandwich", "a tuna salad", "a smoked salmon bagel", "sushi"]
+    let dinner= ["pasta and pesto", "roasted beans and veg", "sweet potatoe chips", "salmon and broccoli", "rissoto"]
 
-function randomNum (num) {
-    return Math.floor(Math.random() * num)
+
+function selector(meal){
+return meal[Math.floor(Math.random()*meal.length)]
 }
-
-    let mealPlan = []
-
-    for (let meal in meals) {
-     let i = randomNum(meals[meal].length)
-    
-    switch(meal) {
-        case 'breakfast':
-          mealPlan.push(`For breakfast eat "${meals[meal][i]}".`)
-          break
-        case 'lunch':
-          mealPlan.push(`for lunch eat: "${meals[meal][i]}".`)
-          break
-        case 'dinner':
-         mealPlan.push(`for dinner eat: "${meals[meal][i]}".`)
-          break
-        default:
-          mealPlan.push('There is not enough info.')
-      }
-    }
+ //console.log(selector(breakfast))   
+ let randomMeal = `For breakfast eat: ${selector(breakfast)} \nFor lunch eat: ${selector(lunch)} \nFor dinner eat: ${selector(dinner)} `
 
   function displayPlan(plan) {
     const ascci= "  :] ^_^ {: \n"
   const emoji = " :p :) <3 n_n \n \n"
   const hearts = "\n <3 <3 <3 <3 <3 <3"
-    const showPlan = mealPlan.join('\n')
+    const showPlan = randomMeal
     console.log(ascci + emoji + showPlan + hearts)
   }
   
-  displayPlan(mealPlan);
+  displayPlan(randomMeal);
